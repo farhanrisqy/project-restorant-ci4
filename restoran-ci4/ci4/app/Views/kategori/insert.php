8dcb18adde227eb80c17974d051a31fb7868bac1
farhanrisqy <70682342+farhanrisqy@users.noreply.github.com>
2020-09-14 11:55:09 +0700

    Tugas Coding Minggu #2
    Project Restorant CI 4 Video (1-11)

---
+++ restoran-ci4/ci4/app/Views/kategori/insert.php
@@ -0,0 +1,17 @@
+<?= $this->extend('template/admin') ?>
+
+<?= $this->section('content') ?>
+
+<h1>Insert Data</h1>
+
+<form action="<?= base_url()?>/admin/kategori/insert" method="post">
+
+    Kategori = <input type="text" name="kategori" required>
+    <br>
+    Keterangan = <input type="text" name="keterangan" required>
+    <br>
+    <input type="submit" value="SIMPAN">
+
+</form>
+
+<?= $this->endSection() ?>
