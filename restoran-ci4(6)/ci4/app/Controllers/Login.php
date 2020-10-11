2ec98edf1053da5c683f641b7fa0a0c5b103782c
farhanrisqy <70682342+farhanrisqy@users.noreply.github.com>
2020-10-11 21:10:13 +0700

    Tugas Coding Minggu #6
    Project Restorant CI 4 Video (30-33)

---
+++ restoran-ci4(6)/ci4/app/Controllers/Login.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class Login extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+		echo "<h1>Belajar CI4</h1>";
+	}
+
+	//--------------------------------------------------------------------
+
+}
